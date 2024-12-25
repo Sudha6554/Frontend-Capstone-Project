6549f3e62c1a5f046ae1e2715bf85528cd3d2b58
Sudha6554 <sudhavurimi@gmail.com>
2024-12-25 15:17:04 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Main />
+        <Menu />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
